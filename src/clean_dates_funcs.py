194b7b991d4561e75c74c0012f3734b78b6eee43
pabloestevezm <papuestev@gmail.com>
2020-07-27 15:42:39 +0200

    modelling

--- src/clean_dates_funcs.py
+++ src/clean_dates_funcs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime
+from statsmodels.tsa.stattools import adfuller
 
 def red_and_ind(df):
     """
@@ -11,15 +12,16 @@ def red_and_ind(df):
     df = df[df['dates'] == True]
     df.index = pd.DatetimeIndex(df.DATE_TIME)
     df = df.drop(columns={'DATE_TIME', 'all_dates', 'dates'})
+    df['DIFF']= df['CLOSE']-df['OPEN']
     return df
 
 
-def get_sample(df):
+def get_sample(df, timestart, timeend):
     """
-    This function to get only December 2019 sample to try season
+    This function get dates to return DF filtered from the clean csv
     """
     df['all_dates'] = pd.to_datetime(df['DATE_TIME'])
-    df['dates'] = (df['all_dates'] > '2019-11-30 23:45:00') & (df['all_dates'] <= '2020-01-01 00:00:00')
+    df['dates'] = (df['all_dates'] > f'{timestart}') & (df['all_dates'] <= f'{timeend}')
     df = df[df['dates'] == True]
     df.index = pd.DatetimeIndex(df.DATE_TIME)
     df = df.drop(columns={'DATE_TIME', 'all_dates', 'dates'})
@@ -31,8 +33,14 @@ def renaming(df, name):
     """
     This function will be useful when we concat differents DFs to help us to difference between pairs
     """
-    df = df.rename(columns={'HIGH':f'HIGH_{name}', 'LOW':f'LOW_{name}', 'OPEN':f'OPEN_{name}', 'CLOSE':f'CLOSE_{name}'})
+    df = df.rename(columns={'HIGH':f'HIGH_{name}', 'LOW':f'LOW_{name}', 'OPEN':f'OPEN_{name}', 'CLOSE':f'CLOSE_{name}', 'DIFF':f'DIFF_{name}'})
     return df
 
 
-#predict_GRAD500 = predict_GRAD500.rename(columns={"index": "id", 0: "price"})
+def stationary_or_not(df):
+    """
+    Apply the adfuller to compare if it's Stationary or not, to test the null hypothesis
+    """
+    stationaryCheck = lambda X: "Not-Stationary" if adfuller(X)[1] > 0.05 else "Stationary"
+    return [(col,stationaryCheck(df[col])) for col in df.columns]
+
