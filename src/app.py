9c5d24a618d82c8858000c81cc21b22cbb9b0435
pabloestevezm <papuestev@gmail.com>
2020-07-31 09:41:23 +0200

    finish

--- src/app.py
+++ src/app.py
@@ -17,14 +17,158 @@ def currencies_opts():
     }
     return currs
 
-"""
-@app.route(f"/currencies_pairs/{currency}")
-def metrics_curr(currency):
-    
+
+@app.route("/currencies_pairs/EUR-USD")
+def metrics_eur_usd():
+    metrics={"AR model": {
+        "MSE":3.614101692169201e-05,
+        "RMSE":0.006007549111511034,
+        "MAE":0.00502701275,
+        "MAPE":0.45287004293971334
+    },
+    "ARIMA":{
+        "MSE":0.00010233317821826405,
+        "RMSE":0.010115986270169807,
+        "MAE":0.009184528267,
+        "MAPE":0.8281034865195951
+    },
+    "LSTM":{
+        "MSE":6.007233489449766e-08,
+        "MAE":0.00019088951835750213
+    }}
+   
+    return metrics
+
+
+@app.route("/currencies_pairs/USD-JPY")
+def metrics_usd_jpy():
+    metrics={"AR model": {
+        "MSE":0.22565868861232463,
+        "RMSE":0.47503546037356476,
+        "MAE":0.3580792364756238,
+        "MAPE":0.3898679934827795
+    },
+    "ARIMA":{
+        "MSE":"no values",
+        "RMSE":"no values",
+        "MAE":"no values",
+        "MAPE":"no values"
+    },
+    "LSTM":{
+        "MSE":0.0023590851498311283,
+        "MAE":0.04665424391429259
+    }}
+   
+    return metrics  
+
+
+@app.route("/currencies_pairs/AUD-USD")
+def metrics_aud_usd():
+    metrics={"AR model": {
+        "MSE":1.741659014777564e-05,
+        "RMSE":0.004173318840895773,
+        "MAE":0.003521097994548128,
+        "MAPE":0.5116864664692757
+    },
+    "ARIMA":{
+        "MSE":0.0001111009846518952,
+        "RMSE":0.010540445182813447,
+        "MAE":0.00947319382991533,
+        "MAPE":1.3715546860492003
+    },
+    "LSTM":{
+        "MSE":1.33865493462109e-06,
+        "MAE":0.0011187356037103862
+    }}
+   
+    return metrics
+
+
+@app.route("/currencies_pairs/EUR-CHF")
+def metrics_eur_chf():
+    metrics={"AR model": {
+        "MSE":0.00030440562070237827,
+        "RMSE":0.017447223868065034,
+        "MAE":0.016289152618739228,
+        "MAPE":1.4930270369196879
+    },
+    "ARIMA":{
+        "MSE":"no values",
+        "RMSE":"no values",
+        "MAE":"no values",
+        "MAPE":"no values"
+    },
+    "LSTM":{
+        "MSE":1.5523767718257138e-06,
+        "MAE":0.0011856212391520159
+    }}
+   
+    return metrics
+
+
+@app.route("/currencies_pairs/EUR-JPY")
+def metrics_eur_jpy():
+    metrics={"AR model": {
+        "MSE":0.5147496953878345,
+        "RMSE":0.7174605880380013,
+        "MAE":0.5765051127688638,
+        "MAPE":0.4769771079143303
+    },
+    "ARIMA":{
+        "MSE":1.2365631493203233,
+        "RMSE":1.112008610272566,
+        "MAE":0.9041489252334723,
+        "MAPE":0.7487021520115805
+    },
+    "LSTM":{
+        "MSE":0.006453482102691483,
+        "MAE":0.06920059000050059
+    }}
+   
+    return metrics
 
 
 
+@app.route("/currencies_pairs/USD-CAD")
+def metrics_usd_cad():
+    metrics={"AR model": {
+        "MSE":0.00015502738279382915,
+        "RMSE":0.012450999268887182,
+        "MAE":0.010627571555471121,
+        "MAPE":0.8092399999695971
+    },
+    "ARIMA":{
+        "MSE":0.00015160379651125056,
+        "RMSE":0.012312749348185829,
+        "MAE":0.010583139335968918,
+        "MAPE":0.8057682571433928
+    },
+    "LSTM":{
+        "MSE":1.219216980426838e-07,
+        "MAE":0.00030136238895184036
+    }}
+   
+    return metrics
 
 
 
-"""
+@app.route("/currencies_pairs/USD-CHF")
+def metrics_usd_chf():
+    metrics={"AR model": {
+        "MSE":0.00019715294563072764,
+        "RMSE":0.014041116253016626,
+        "MAE":0.012984486520519279,
+        "MAPE":1.3240289129443967
+    },
+    "ARIMA":{
+        "MSE":0.00017326048154725095,
+        "RMSE":0.01316284473612186,
+        "MAE":0.011822151938955892,
+        "MAPE":1.2060969870079636
+    },
+    "LSTM":{
+        "MSE":2.127671385084142e-07,
+        "MAE":0.00040612818377459684
+    }}
+   
+    return metrics
--- src/modelling_funcs.py
+++ src/modelling_funcs.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import MinMaxScaler
 import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Input, LSTM, Dense, Flatten, Conv2D, MaxPooling2D
-from cleaning_functions import *
 
 
 def stationary_or_not(df):
@@ -78,12 +77,12 @@ def modelling_ARIMA(df, name):
         train = realidad[col]
         test = futuro[col]
 
-        # Entrena el modelo AR
+        # Entrena el modelo ARIMA
         model_ARIMA = ARIMA(train, order=(0,0,1))
         print(f"Entrenando con los datos desde la serie {col}")
         model_fit_ARIMA = model_ARIMA.fit(maxlag=4)
         
-        # Predice los valores AR
+        # Predice los valores ARIMA
         predictions_ARIMA[f'{col}_prediction'] = model_fit_ARIMA.predict(start=len(train),
                                         end=len(train)+len(test)-1, dynamic=False)
       
@@ -103,25 +102,29 @@ def modelling_ARIMA(df, name):
 
 
 def create_dataset(df, name, look_back=10):
+    """
+    This function is for giving my df the 3d with the timesteps to calculate the NN
+    """
     df_copy = df.copy()
     df_copy = df_copy.drop(f'close_{name}', axis=1)
     dataX, dataY = [], []
     for i in range(len(df)-look_back-1):
         a = df[i:(i+look_back)]
         dataX.append(a)
-        #dataY.append(df.iloc[i+look_back])
     return np.array(dataX), np.array(dataY)
 
 
 
 def modelling_LSTM(df, name, look_back=4):
-    #df2 = df.iloc[:,1:5]
+    x_scaler = MinMaxScaler(feature_range=(0, 1))
+    x_scaler = x_scaler.fit(df.values.astype('float32'))
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+    y_scaler = y_scaler.fit(df[f'close_{name}'].values.astype('float32').reshape(-1,1))
     scaler = MinMaxScaler()
     scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
     X, y = create_dataset(scaled, name, look_back)
-    print(X.shape, X)
+    #print(X.shape, X)
     y = scaled[look_back:-1]
-    #X = scaled.drop(f'close_{name}', axis=1)
     y = scaled[f'close_{name}']
     SPLIT = 0.9
     train_size = int(len(X) * SPLIT)
@@ -134,9 +137,12 @@ def modelling_LSTM(df, name, look_back=4):
     model.add(LSTM(10, return_sequences=True))
     model.add(Dropout(0.2))
     model.add(Dense(1, kernel_initializer='uniform', activation='relu'))
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae', 'mse'])
-    model_LSTM = model.fit(X_train, y_train, epochs=5, batch_size=1)
-    return model_LSTM
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
+    model_LSTM = model.fit(X_train, y_train, epochs=4, batch_size=1)
+    return model_LSTM, model, X_test, y_test, y_scaler
+
+
+
 
 
 
@@ -160,9 +166,24 @@ def get_metrics(df, df_orig, name):
     return mse, rmse, mape, mae
     
 
-  
-eur_usd = pd.read_csv('./Input/EURUSD-2000-2020-15m.csv')
-eur_usd = red_LSTM(eur_usd, 'EUR-USD', '2019-12-01 23:45:00','2020-01-01 00:00:00')
-print(eur_usd)
-a= modelling_LSTM(eur_usd, 'EUR-USD')
 
+def get_pred_LSTM(model_LSTM, model, X_test, y_test, y_scaler):
+    pred = model.predict(X_test)
+    prediction_np = np.reshape(pred, (2478, 4))
+    y_test2 = y_test.values.reshape(2483,1)
+    prediction = pd.DataFrame(prediction_np)
+    predict = []
+    for i in range(len(prediction)):
+        x = (prediction.iloc[i].sum())/(len(prediction.iloc[i]))
+        predict.append(x)
+    
+    prediction['pred']= predict
+    prediction_clean = prediction.drop(columns={0,1,2,3})
+    pred = y_scaler.inverse_transform(prediction_clean)
+    close = y_scaler.inverse_transform(y_test2)
+
+    predictions = pd.DataFrame()
+    predictions['predicted'] = pd.Series(np.reshape(pred, (pred.shape[0])))
+    predictions['GT'] = pd.Series(np.reshape(close, (close.shape[0])))
+    
+    return predictions
